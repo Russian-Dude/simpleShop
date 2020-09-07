cf4ae9e23b00fdb0687f6d222362289797daae05
TheRussianDude <hritnik@yandex.ru>
2020-09-07 22:27:16 +0300

    Start project

---
+++ src/test/java/ru/rdude/simpleStore/SimpleStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rdude.simpleStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
